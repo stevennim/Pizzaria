426b15d3fec6b9cd6f739b21a060050a4a7461f0
Steven Nim <stevennim1@gmail.com>
2019-01-07 22:45:09 -0500

    Spruced up home page

--- app/src/main/java/com/hfad/pizzasupplibraryandappbars/MainActivity.java
+++ app/src/main/java/com/hfad/pizzasupplibraryandappbars/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.MenuItem;
 import android.content.Intent;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.design.widget.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     private ShareActionProvider sap;
@@ -18,8 +23,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //set this activity's app bar
-        Toolbar t = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(t);
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+
+        //attach the SectionsPagerAdapter (allows fragments to be on pagers)
+        //to the ViewPager so fragments can be displayed on the app's view pager
+        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+
+        //Attach the ViewPager to the TabLayout so each section can be accessed by the tabs or by swiping
+        TabLayout t = (TabLayout) findViewById(R.id.tabs);
+        t.setupWithViewPager(pager);
     }
 
     @Override
@@ -49,6 +64,48 @@ public class MainActivity extends AppCompatActivity {
 
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //this class is used to populate each section of the pager with a different fragment
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public SectionsPagerAdapter(FragmentManager fm){
+            super(fm);
+        }
+
+        @Override
+        public int getCount(){//there are 4 pages in the view pager
+            return 4;
+        }
+
+        @Override
+        public Fragment getItem(int pos){//assigns positions for each fragment to be displayed in the pager
+            switch(pos){
+                case 0:
+                    return new TopFragment();
+                case 1:
+                    return new PizzaFragment();
+                case 2:
+                    return new PastaFragment();
+                case 3:
+                    return new StoresFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){//adds text to each tab
+            switch(position){
+                case 0:
+                    return getResources().getText(R.string.home_tab);
+                case 1:
+                    return getResources().getText(R.string.pizzas_tab);
+                case 2:
+                    return getResources().getText(R.string.pasta_tab);
+                case 3:
+                    return getResources().getText(R.string.stores_tab);
+            }
+            return null;
         }
     }
 }
